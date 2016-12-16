9afb99321868dcdcc60aa754aadd74625d2ece6f
insperclicker <jppc9292@gmail.com>
2016-12-16 16:03:41 -0200

    sprint 2

--- app/src/main/java/br/edu/insper/memefy/MainActivity.java
+++ app/src/main/java/br/edu/insper/memefy/MainActivity.java
@@ -1,5 +1,6 @@
 package br.edu.insper.memefy;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
@@ -10,18 +11,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     int TAKE_PHOTO_CODE = 0;
     public static int count = 0;
+    final int PIC_CROP = 2;
+    File newfile;
+    String file;
+
+    //Uri outputFileUri;
+    ArrayList<ImageView> listMemes;
+
     ImageView iw1;
     ImageView iw2;
     ImageView iw3;
     ImageView iw4;
+    ImageView iw5;
+    ImageView idply;
 
 
     @Override
@@ -33,35 +45,69 @@ public class MainActivity extends AppCompatActivity {
         File newdir = new File(dir);
         newdir.mkdirs();
         iw1=(ImageView) findViewById(R.id.meme1);
+        iw2=(ImageView) findViewById(R.id.meme2);
+        iw3=(ImageView) findViewById(R.id.meme3);
+        iw4=(ImageView) findViewById(R.id.meme4);
+        iw5=(ImageView) findViewById(R.id.meme5);
+        idply=(ImageView) findViewById(R.id.memeDisplay);
+
 
         Button capture = (Button) findViewById(R.id.captureBtn);
         capture.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                count++;
-                String file = dir+count+".jpg";
-                File newfile = new File(file);
+
+                file = dir+count+".jpg";
+                newfile = new File(file);
                 try {
                     newfile.createNewFile();
-                }
-                catch (IOException e)
-                {
-                }
+               }
+                    catch (IOException e)
+               {
+               }
 
                 Uri outputFileUri = Uri.fromFile(newfile);
 
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,outputFileUri );
 
                 startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
             }
         });
 
-        final ImageView imageBtn = (ImageView) findViewById(R.id.meme1);
-        imageBtn.setOnClickListener(new View.OnClickListener() {
+        final ImageView imageBtn1 = (ImageView) findViewById(R.id.meme1);
+        imageBtn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("image clicked...");
-                iw1.setImageResource(R.drawable.meme3);
+                idply.setImageDrawable(iw1.getDrawable());
+            }
+        });
+        final ImageView imageBtn2 = (ImageView) findViewById(R.id.meme2);
+        imageBtn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("image clicked...");
+                idply.setImageDrawable(iw2.getDrawable());
+            }
+        });
+        final ImageView imageBtn3 = (ImageView) findViewById(R.id.meme3);
+        imageBtn3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("image clicked...");
+                idply.setImageDrawable(iw3.getDrawable());
+            }
+        });
+        final ImageView imageBtn4 = (ImageView) findViewById(R.id.meme4);
+        imageBtn4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("image clicked...");
+                idply.setImageDrawable(iw4.getDrawable());
+            }
+        });
+        final ImageView imageBtn5 = (ImageView) findViewById(R.id.meme5);
+        imageBtn5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("image clicked...");
+                idply.setImageDrawable(iw5.getDrawable());
             }
         });
 
@@ -75,6 +121,49 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
             Log.d("CameraDemo", "Pic saved");
+            performCrop();
         }
+
+        else if(requestCode == PIC_CROP && resultCode == RESULT_OK){
+            Log.d("CameraCrop",("Pic Croped"));
+        }
+
+
+    }
+
+    public void performCrop(){
+        try {
+            final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+            count++;
+            String file = dir+count+".jpg";
+            File newfile = new File(file);
+            try {
+                newfile.createNewFile();
+             }
+             catch (IOException e)
+             {
+             }
+
+            Uri outputFileUri = Uri.fromFile(newfile);
+
+
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(outputFileUri, "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            cropIntent.putExtra("return-data", true);
+            cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            startActivityForResult(cropIntent, PIC_CROP);
+
+        }
+        catch(ActivityNotFoundException anfe){
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 }
